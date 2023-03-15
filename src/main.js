528eb1e6beac4ab0c63eddf6ca795e9f4e34235b
ivelov-vm <ivelov_vm@groupbwt.com>
2023-03-15 11:21:25 +0200

    feat: add tailwind and router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import '../dist/output.css'
+import router from './plugins/router'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
